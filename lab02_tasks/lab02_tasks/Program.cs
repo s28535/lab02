c37895985acdc2393da022bde246324e7979648e
s28535 <s28535@pjwstk.edu.pl>
2024-03-11 13:07:12 +0100

    Modyfikacja 2

--- lab02_tasks/lab02_tasks/Program.cs
+++ lab02_tasks/lab02_tasks/Program.cs
@@ -3,5 +3,10 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello Git!");
+
+        int a = 5;
+        int b = 8;
+        int c = a + b;
+        Console.WriteLine(c);
     }
 }
